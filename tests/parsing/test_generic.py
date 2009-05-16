03d12582d7e5df83b8e9e517f53ba181a1ef3b45
Gustavo Narea <me@gustavonarea.net>
2009-05-17 01:15:04 +0200

    - Turned the "booleano.parser.testutil" module into the "booleano.parser.testutils" package. - Moved the mock parse tree elements from the testutils module itself into their own module: booleano.parser.testutils.tree. - Implemented the base class for the operations.

--- tests/parsing/test_generic.py
+++ tests/parsing/test_generic.py
@@ -33,8 +33,8 @@ Test suite for the generic grammar.
 from nose.tools import eq_, ok_, assert_false, assert_raises
 
 from booleano.parser import GenericGrammar
-from booleano.parser.testutil import (BaseParseTest, STRING, NUMBER, VARIABLE,
-                                      SET)
+from booleano.parser.testutils import BaseParseTest
+from booleano.parser.testutils.tree import (STRING, NUMBER, VARIABLE, SET)
 from booleano.operations.operators import (FunctionOperator, TruthOperator,
         NotOperator, AndOperator, OrOperator, XorOperator, EqualOperator,
         LessThanOperator, GreaterThanOperator, LessEqualOperator, 
