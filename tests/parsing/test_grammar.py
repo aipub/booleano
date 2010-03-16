a4068d063973dfd3cc8003598736a72baa5fb580
Gustavo Narea <me@gustavonarea.net>
2010-03-16 23:24:56 +0000

    Updated license header in the Python files

--- tests/parsing/test_grammar.py
+++ tests/parsing/test_grammar.py
@@ -4,7 +4,7 @@
 #
 # This file is part of Booleano <http://booleano.efous.org/>.
 #
-# This program respects your freedom: you can redistribute it and/or modify it
+# This program is Freedomware: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
 # later version.
